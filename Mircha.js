c3a1086395cd035dc9ca4bf87d7b5a58c14c5c90
Erick-SN <ericksn0707@gmail.com>
2021-01-07 20:06:54 -0600

    Destructuracion

--- Mircha.js
+++ Mircha.js
@@ -1,1 +1,15 @@
-// -- Caracteristicas y Gramatica -- //
+// -- Repaso General de JS  -- //
+
+//--Destructuracion
+
+let numeros = [1, 2, 3];
+const [uno, dos, tres] = numeros;
+
+let persona = {
+  nombre: 'Erick',
+  apellido: 'Sanchez',
+  edad: 22,
+};
+
+let { nombre } = persona;
+console.log(nombre);
