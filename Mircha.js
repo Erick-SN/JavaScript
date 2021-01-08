ed1514981cda75cdd7f06a17a72943b836a8ce4d
Erick-SN <ericksn0707@gmail.com>
2021-01-08 17:45:52 -0600

    POO

--- Mircha.js
+++ Mircha.js
@@ -30,3 +30,32 @@ const arr = [1, 2, 3, 4, 5, 6, 7, 8];
 const arrDos = [9, 10, 11, 12, 13, 14, 15];
 const arrTres = [...arr, ...arrDos]; // spread
 console.log(arrTres);
+
+//-- Clses de JS
+
+class Animal {
+  constructor(nombre, genero) {
+    this.nombre = nombre;
+    this.genero = genero;
+  }
+  sonar() {
+    console.log('Estoy vivo');
+  }
+  saludar() {
+    console.log(`Hola mi nombre es ${this.nombre}`);
+  }
+}
+
+class Perro extends Animal {
+  constructor(nombre, genero, talla) {
+    super(nombre, genero);
+    this.talla = talla;
+  }
+  sonar() {
+    console.log('Wau wau');
+  }
+}
+
+const Canela = new Perro('Canela', 'Hembra', 'Chica');
+Canela.saludar();
+Canela.sonar();
