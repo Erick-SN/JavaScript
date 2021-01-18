69b4f1ced4a494e42033b6f572d12b78d85413f1
Erick-SN <ericksn0707@gmail.com>
2021-01-18 11:43:06 -0600

    promises

--- Asincronia.js
+++ Asincronia.js
@@ -24,3 +24,54 @@ getTodos((err, data) => {
     console.log(data);
   }
 });
+
+// Promise example
+
+const getSomething = () => {
+  return new Promise((resolve, reject) => {
+    resolve('some date');
+    reject('some error');
+  });
+};
+
+getSomething().then(
+  (data) => {
+    console.log(data);
+  },
+  (err) => {
+    console.log(err);
+  }
+);
+
+getSomething()
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const getTodosP = (url) => {
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
+    reques.addEventListener('readystatechange', () => {
+      //console.log(request, request.readyState);
+      if (request.readyState === 4 && request.status === 200) {
+        const data = JSON.parse(request.responseText);
+        resolve(data);
+      } else if (request.readyState === 4) {
+        reject('some error occurred');
+      }
+    });
+    request.open('GET', url);
+    reques.send();
+  });
+};
+
+getTodosP('someurl')
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
