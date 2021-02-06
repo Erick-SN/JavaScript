6ab88432717293e72397fd78d747f9a03c7a34c2
Erick-SN <ericksn0707@gmail.com>
2021-02-06 13:33:15 -0600

    teclados

--- dom/index.js
+++ dom/index.js
@@ -1,6 +1,6 @@
 import hamburgerMenu from './menu.js';
 import { digitalClock, alarm } from './reloj.js';
-import { shortCuts } from './teclado.js';
+import { shortCuts, moveBall } from './teclado.js';
 
 const d = document;
 
@@ -12,4 +12,5 @@ d.addEventListener('DOMContentLoaded', (e) => {
 
 d.addEventListener('keydown', (e) => {
   shortCuts(e);
+  moveBall(e, '.ball', '.stage');
 });
--- dom/teclado.js
+++ dom/teclado.js
@@ -1,6 +1,37 @@
 const d = document;
+let x = 0,
+  y = 0;
 
 export const shortCuts = (e) => {
   if (e.key === 'a' && e.altKey) alert('Holi crayoli');
   if (e.key === 'c' && e.altKey) confirm('Holi crayoli x2');
+};
+
+export const moveBall = (e, ball, stage) => {
+  const $ball = d.querySelector(ball);
+  const $stage = d.querySelector(stage);
+  const limitBall = $ball.getBoundingClientRect();
+  const limitStage = $stage.getBoundingClientRect();
+
+  switch (e.keyCode) {
+    case 37:
+      e.preventDefault();
+      if (limitBall.left > limitStage.left) x--;
+      break;
+    case 38:
+      e.preventDefault();
+      if (limitBall.top > limitStage.top) y--;
+      break;
+    case 39:
+      e.preventDefault();
+      if (limitBall.right < limitStage.right) x++;
+      break;
+    case 40:
+      e.preventDefault();
+      if (limitBall.bottom < limitStage.bottom) y++;
+      break;
+    default:
+      break;
+  }
+  $ball.style.transform = `translate(${x * 10}px,${y * 10}px)`;
 };
