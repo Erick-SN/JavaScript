c35696591f475c08a19dda8029006acb653d1193
Erick-SN <ericksn0707@gmail.com>
2021-02-06 17:19:23 -0600

    Scroll top

--- dom/index.js
+++ dom/index.js
@@ -1,5 +1,6 @@
 import hamburgerMenu from './menu.js';
 import countDown from './cuentaRegresiva.js';
+import scrollTop from './scroll.js';
 import { digitalClock, alarm } from './reloj.js';
 import { shortCuts, moveBall } from './teclado.js';
 
@@ -10,6 +11,7 @@ d.addEventListener('DOMContentLoaded', (e) => {
   digitalClock('#reloj', '#activar-reloj', '#desactivar-reloj');
   alarm('./assets/ara.mp3', '#activar-alarma', '#desactivar-alarma');
   countDown('countDown', 'May 23,2021 03:23:19', 'Fin');
+  scrollTop('.scroll-top-button');
 });
 
 d.addEventListener('keydown', (e) => {
