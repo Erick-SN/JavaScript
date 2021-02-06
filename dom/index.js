bbcf631ec3c64b807a589dbc5f8d2398e2229c7b
Erick-SN <ericksn0707@gmail.com>
2021-02-06 11:34:45 -0600

    reloj

--- dom/index.js
+++ dom/index.js
@@ -1,6 +1,9 @@
 import hamburgerMenu from './menu.js';
+import { digitalClock, alarm } from './reloj.js';
+
 const d = document;
 
 d.addEventListener('DOMContentLoaded', (e) => {
   hamburgerMenu('.panel-btn', '.panel', '.menu a');
+  digitalClock('#reloj', '#activar-reloj', '#desactivar-reloj');
 });
