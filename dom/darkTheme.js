3d2bd2eae83d91d39db90c9c36e835e28a081ecb
Erick-SN <ericksn0707@gmail.com>
2021-02-07 16:13:02 -0600

    localstorage

--- dom/darkTheme.js
+++ dom/darkTheme.js
@@ -1,18 +1,36 @@
 const d = document;
+const local = localStorage;
 export default function darkTheme(btn, classDark) {
   const $selector = d.querySelectorAll('[data-dark]');
   const $themeBtn = d.querySelector(btn);
   let moon = '🌙',
     sun = '☀️';
+
+  const lightMode = () => {
+    $selector.forEach((el) => el.classList.remove(classDark));
+    $themeBtn.textContent = moon;
+    local.setItem('theme', 'light');
+  };
+
+  const darkMode = () => {
+    $selector.forEach((el) => el.classList.add(classDark));
+    $themeBtn.textContent = sun;
+    local.setItem('theme', 'dark');
+  };
+
   d.addEventListener('click', (e) => {
     if (e.target.matches(btn)) {
       if ($themeBtn.textContent === moon) {
-        $selector.forEach((el) => el.classList.add(classDark));
-        $themeBtn.textContent = sun;
+        darkMode();
       } else {
-        $selector.forEach((el) => el.classList.remove(classDark));
-        $themeBtn.textContent = moon;
+        lightMode();
       }
     }
+  });
+
+  d.addEventListener('DOMContentLoaded', (e) => {
+    if (local.getItem('theme') === null) local.setItem('theme', 'light');
+    if (local.getItem('theme') === 'light') lightMode();
+    if (local.getItem('theme') === 'dark') darkMode();
   });
 }
--- dom/index.js
+++ dom/index.js
@@ -13,10 +13,11 @@ d.addEventListener('DOMContentLoaded', (e) => {
   alarm('./assets/ara.mp3', '#activar-alarma', '#desactivar-alarma');
   countDown('countDown', 'May 23,2021 03:23:19', 'Fin');
   scrollTop('.scroll-top-button');
-  darkTheme('.dark-theme-btn', 'dark-mode');
 });
 
 d.addEventListener('keydown', (e) => {
   shortCuts(e);
   moveBall(e, '.ball', '.stage');
 });
+
+darkTheme('.dark-theme-btn', 'dark-mode');
