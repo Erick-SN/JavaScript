e257a0220d61cc07a311da2b0d37d476228c4fef
Erick-SN <ericksn0707@gmail.com>
2021-02-07 18:08:05 -0600

    responsive

--- dom/index.js
+++ dom/index.js
@@ -2,6 +2,7 @@ import hamburgerMenu from './menu.js';
 import countDown from './cuentaRegresiva.js';
 import scrollTop from './scroll.js';
 import darkTheme from './darkTheme.js';
+import responsiveMedia from './responsive.js';
 import { digitalClock, alarm } from './reloj.js';
 import { shortCuts, moveBall } from './teclado.js';
 
@@ -13,6 +14,14 @@ d.addEventListener('DOMContentLoaded', (e) => {
   alarm('./assets/ara.mp3', '#activar-alarma', '#desactivar-alarma');
   countDown('countDown', 'May 23,2021 03:23:19', 'Fin');
   scrollTop('.scroll-top-button');
+  responsiveMedia(
+    'youtube',
+    '(min-width: 1024px)',
+    `<a href='https://www.youtube.com/embed/h9IK0mtfBLA' target='_blank'>Ver Video</a>`,
+    `<iframe width="560" height="315" src="https://www.youtube.com/embed/h9IK0mtfBLA" 
+    frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+    allowfullscreen></iframe>`
+  );
 });
 
 d.addEventListener('keydown', (e) => {
