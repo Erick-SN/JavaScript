e2c94a3ba8742880c28bfa3e11f01b6e55e86b81
Erick-SN <ericksn0707@gmail.com>
2021-03-22 16:32:10 -0600

    Promise.all

--- Async/async.js
+++ Async/async.js
@@ -11,10 +11,31 @@
 //   });
 
 // Async and Await
-const getTodos = async () => {
-  const response = await fetch('./todos/a.json');
-  const data = await response.json();
-  return data;
+
+const getData = (URL) => {
+  return new Promise((resolve, reject) => {
+    const data = fetch(URL).then((response) => {
+      if (response.status !== 200) reject('Error');
+      else response.json().then((data) => resolve(data));
+    });
+    return data;
+  });
 };
 
-getTodos().then((data) => console.log(data));
+const getTodos = async () => {
+  // const response = await fetch('./todos/a.json');
+  // const responseB = await fetch('./todos/b.json');
+  // const responseC = await fetch('./todos/c.json');
+  // const data = await response.json();
+  // const dataB = await responseB.json();
+  // const dataC = await responseC.json();
+  const response = await Promise.all([
+    getData('./todos/a.json'),
+    getData('./todos/b.json'),
+    getData('./todos/c.json'),
+  ]);
+  return response;
+  // return [data, dataB, dataC];
+};
+
+getTodos().then((data) => data.map((blogs) => console.log(blogs)));
